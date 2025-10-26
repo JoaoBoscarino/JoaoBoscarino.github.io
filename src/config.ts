add06f97e9aba8e175dbe720149106ed09deea85
Joao Boscarino <joaoboscarino434@gmail.com>
2025-10-26 18:28:31 -0300

    Criando meu portifólio

---
+++ src/config.ts
@@ -0,0 +1,50 @@
+export const siteConfig = {
+  name: "João Boscarino",
+  title: "Web Developer",
+  description: "Portfólio pessoal mostrando meus projetos e habilidades em desenvolvimento web",
+  accentColor: "#ffffffff", // cor azul padrão, você pode alterar
+  social: {
+    email: "joaoboscarino434@gmail.com",
+    linkedin: "https://www.linkedin.com/in/joaoboscarino",
+    github: "https://github.com/JoaoBoscarino",
+  },
+ aboutMe:
+  "Olá! Sou João, um desenvolvedor web apaixonado por criar projetos modernos e funcionais. Tenho experiência com desenvolvimento front-end e back-end, sempre buscando aprender novas tecnologias e melhorar minhas habilidades. Gosto de desenvolver aplicações web responsivas e sistemas integrados.",
+skills: ["HTML", "CSS", "JavaScript"],
+
+
+  projects: [
+    {
+      name: "Nexify Store",
+      description:
+        "Loja virtual totalmente interativa, desenvolvida com HTML, CSS e JavaScript.",
+      link: "https://github.com/JoaoBoscarino/Nexify-Store",
+      skills: ["HTML", "CSS", "JavaScript"]
+    }
+
+  ],
+  experience: [
+    {
+      company: "Projetos Pessoais e Acadêmicos",
+      title: "Desenvolvedor Web",
+      dateRange: "2025 - Presente",
+      bullets: [
+        "Criação de sistemas web completos usando HTML, CSS e JavaScript",
+        "Implementação de layouts responsivos e modernos"
+      ]
+    }
+
+  ],
+
+  education: [
+    {
+      school: "ETERJ",
+      degree: "Ensino Médio Técnico em Informática",
+      dateRange: "2022 - 2024",
+      achievements: [
+        "Participação em projetos de tecnologia da escola"
+      ]
+    }
+
+  ]
+};
